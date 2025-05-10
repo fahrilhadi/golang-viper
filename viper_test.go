9d9fce254d552a690556ec4f48c9e621b20dcba8
Fahril Hadi <fahrilhadi77@gmail.com>
2025-05-10 20:37:59 +0700

    membuat viper

---
+++ viper_test.go
@@ -0,0 +1,13 @@
+package golang_viper
+
+import (
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestViper(t *testing.T)  {
+	var config *viper.Viper = viper.New()
+	assert.NotNil(t, config)
+}
